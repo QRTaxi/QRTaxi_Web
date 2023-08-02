086b14df76959a1cb2a3d685afe6cdc1cd06e0fc
HiImConan <mindol0320@naver.com>
2023-08-03 00:07:55 +0900

    Rename: lottie branch 이후 remote/main 변경사항 waiting-page branch로rebase

--- src/components/common/Router.tsx
+++ src/components/common/Router.tsx
@@ -3,10 +3,11 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 
 const Landing = lazy(() => import('src/pages/landing'));
-const Finish = lazy(() => import('src/pages/finish'));
 const Booking = lazy(() => import('src/pages/booking'));
+const Waiting = lazy(() => import('src/pages/waiting'));
 const Cancel = lazy(() => import('src/pages/cancel'));
 const NoTaxi = lazy(() => import('src/pages/noTaxi'));
+const Finish = lazy(() => import('src/pages/finish'));
 
 const Router = () => (
   <BrowserRouter>
@@ -14,10 +15,11 @@ const Router = () => (
       <Suspense>
         <Routes>
           <Route path="/" element={<Landing />} />
-          <Route path="/finish" element={<Finish />} />
           <Route path="/booking" element={<Booking />} />
+          <Route path="/waiting" element={<Waiting />} />
           <Route path="/cancel" element={<Cancel />} />
           <Route path="/noTaxi" element={<NoTaxi />} />
+          <Route path="/finish" element={<Finish />} />
         </Routes>
       </Suspense>
     </RecoilRoot>
