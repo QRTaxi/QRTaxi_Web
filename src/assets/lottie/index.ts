cf11aa9f349948b340aec155480bbb83da43782a
zizonyoungjun <naya700@naver.com>
2023-08-02 17:44:25 +0900

    feat: lottie json 추가

--- src/assets/lottie/index.ts
+++ src/assets/lottie/index.ts
@@ -1,1 +1,4 @@
 export { default as Taxi } from './taxi.json';
+export { default as HighFive } from './highFive.json';
+export { default as Cancel } from './cancel.json';
+export { default as NoTaxi } from './noTaxi.json';
