31de92feed2aa735ef696f392f1ea40a23a977d1
zizonyoungjun <naya700@naver.com>
2023-08-02 21:12:08 +0900

    fix: lottie index 수정

--- src/assets/lottie/index.ts
+++ src/assets/lottie/index.ts
@@ -1,4 +1,4 @@
 export { default as Taxi } from './taxi.json';
 export { default as HighFive } from './highFive.json';
-export { default as Cancel } from './cancel.json';
+export { default as Cancelled } from './cancelled.json';
 export { default as NoTaxi } from './noTaxi.json';
