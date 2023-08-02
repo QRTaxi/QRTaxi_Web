cb823d50850a4021bf1efa8735601415edddbe1b
HiImConan <mindol0320@naver.com>
2023-08-03 00:07:55 +0900

    Rename: Booking -> Waiting 페이지 네이밍 수정

--- src/components/common/Router.tsx
+++ src/components/common/Router.tsx
@@ -3,8 +3,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 
 const Landing = lazy(() => import('src/pages/landing'));
-const Finish = lazy(() => import('src/pages/finish'));
 const Booking = lazy(() => import('src/pages/booking'));
+const Finish = lazy(() => import('src/pages/finish'));
+const Waiting = lazy(() => import('src/pages/waiting'));
 
 const Router = () => (
   <BrowserRouter>
@@ -12,8 +13,9 @@ const Router = () => (
       <Suspense>
         <Routes>
           <Route path="/" element={<Landing />} />
-          <Route path="/finish" element={<Finish />} />
           <Route path="/booking" element={<Booking />} />
+          <Route path="/waiting" element={<Waiting />} />
+          <Route path="/finish" element={<Finish />} />
         </Routes>
       </Suspense>
     </RecoilRoot>
