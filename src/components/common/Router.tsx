b6a468dbc0165e370cef52a2000d8b56622f652e
HiImConan <mindol0320@naver.com>
2023-08-05 00:48:14 +0900

    Feat: reactrouter - createBrowserRouter 도입

--- src/components/common/Router.tsx
+++ src/components/common/Router.tsx
@@ -1,29 +1,38 @@
-import { lazy, Suspense } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { RecoilRoot } from 'recoil';
+import { createBrowserRouter } from 'react-router-dom';
 
-const Landing = lazy(() => import('src/pages/landing'));
-const Booking = lazy(() => import('src/pages/booking'));
-const Waiting = lazy(() => import('src/pages/waiting'));
-const Cancel = lazy(() => import('src/pages/cancel'));
-const NoTaxi = lazy(() => import('src/pages/noTaxi'));
-const Finish = lazy(() => import('src/pages/finish'));
+import Landing from '@/pages/landing';
+import Booking, { Loader as bookingLoader } from '@/pages/booking';
+import Waiting from '@/pages/waiting';
+import Cancel from '@/pages/cancel';
+import Finish from '@/pages/finish';
+import NoTaxi from '@/pages/noTaxi';
 
-const Router = () => (
-  <BrowserRouter>
-    <RecoilRoot>
-      <Suspense>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/booking" element={<Booking />} />
-          <Route path="/waiting" element={<Waiting />} />
-          <Route path="/cancel" element={<Cancel />} />
-          <Route path="/noTaxi" element={<NoTaxi />} />
-          <Route path="/finish" element={<Finish />} />
-        </Routes>
-      </Suspense>
-    </RecoilRoot>
-  </BrowserRouter>
-);
+const Router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Landing />,
+  },
+  {
+    path: 'booking/:qrID',
+    element: <Booking />,
+    loader: bookingLoader,
+  },
+  {
+    path: 'waiting',
+    element: <Waiting />,
+  },
+  {
+    path: 'cancel',
+    element: <Cancel />,
+  },
+  {
+    path: 'notaxi',
+    element: <NoTaxi />,
+  },
+  {
+    path: 'finish',
+    element: <Finish />,
+  },
+]);
 
 export default Router;
