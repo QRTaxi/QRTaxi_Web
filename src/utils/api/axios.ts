8f20cb594dc9956b8bae85463c13ae4b67ce9e3b
HiImConan <mindol0320@naver.com>
2023-08-05 21:41:27 +0900

    Fix: VITE 환경변수 정상 적용

--- src/utils/api/axios.ts
+++ src/utils/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: import.meta.env.BASE_URL, // vite 환경변수 접근법
+  baseURL: import.meta.env.VITE_BASE_URL as string, // vite 환경변수 접근법
 });
 
 export default client;
