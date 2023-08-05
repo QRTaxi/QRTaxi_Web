09dfb63ce2725a624be78159fad651179eff8fde
HiImConan <mindol0320@naver.com>
2023-08-05 21:55:58 +0900

    Refactor: booking 페이지 비즈니스 로직 리팩토링

--- src/pages/booking/index.tsx
+++ src/pages/booking/index.tsx
@@ -1,37 +1,45 @@
-import { useState } from 'react';
+import { useLayoutEffect, useState } from 'react';
 
 import * as styles from './BookingStyle';
 import Header from '@/components/common/Header';
 import Button from '@/components/common/Button';
 
-import { Params, useLoaderData } from 'react-router-dom';
+import {
+  Params,
+  useLoaderData,
+  useNavigate,
+  useParams,
+} from 'react-router-dom';
 
-import UserApi from '@/utils/api/user';
-import { useRecoilState, useRecoilValue } from 'recoil';
+import { useRecoilState } from 'recoil';
 import { userQRIDState } from '@/utils/recoil/store';
-import { UserLocationInfo } from '@/utils/types/user';
+import { UserLocationResponse, UserQRID } from '@/utils/types/user';
+import UserApi from '@/utils/api/user';
 
-export function Loader({ params }: { params: Params }) {
-  const [userQRID, setUserQRID] = useRecoilState(userQRIDState);
-  if (params.qrID) {
-    setUserQRID(params.qrID);
-    return UserApi.getUserLocation(userQRID);
-  }
-  return null;
+export async function Loader({ params }: { params: Params }) {
+  return UserApi.getUserLocation(params.qrID as string);
 }
 
 const Booking = () => {
-  const userLocationInfo = useLoaderData() as UserLocationInfo;
-  const userQRID = useRecoilValue(userQRIDState);
-  const [phoneNum, setPhoneNum] = useState<number>();
+  const userLocationInfo = useLoaderData() as UserLocationResponse;
+  const { qrID } = useParams();
+  const [userQRID, setUserQRID] = useRecoilState<UserQRID>(userQRIDState);
+  const [phoneNum, setPhoneNum] = useState<string>();
+  const navigate = useNavigate();
+
+  useLayoutEffect(() => {
+    if (qrID) {
+      setUserQRID(qrID);
+    }
+  }, []);
 
   const Validation = (e: React.ChangeEvent<HTMLInputElement>) => {
     // 유효성 검사에서 구현해야할 것들:
     // 1. 11자가 모두 입력됐는지
     // 2. 11자 이상 입력하면 막히게 해야 함
     // 3. 서버에서 제시한대로 정규화해서 보내줘야함(Post)
-    console.log(e.target.value);
-    const num = Number(e.target.value);
+    const num = e.target.value;
+    console.log(num);
     setPhoneNum(num);
     if (phoneNum && phoneNum.toString().length == 11) {
       return true;
@@ -39,9 +47,18 @@ const Booking = () => {
     return false;
   };
 
-  const handleSubmit = () => {
-    const payload = { hashed_qr_id: userQRID, user_phone: phoneNum };
-    return payload;
+  const handleSubmit = async () => {
+    try {
+      const payload = {
+        hashed_qr_id: userQRID as string,
+        user_phone: String(phoneNum),
+      };
+      const response = await UserApi.postUserInfo(payload);
+      // ws 들어갈 자리
+      if (response) navigate('/waiting');
+    } catch (error) {
+      console.error('Failed to submit user info:', error);
+    }
   };
 
   return (
@@ -64,7 +81,12 @@ const Booking = () => {
         </styles.SecondContent>
       </styles.SecondSection>
       <styles.ButtonSection>
-        <Button text="호출하기" onClick={handleSubmit} />
+        <Button
+          text="호출하기"
+          onClick={() => {
+            handleSubmit().catch(error => console.error(error));
+          }}
+        />
       </styles.ButtonSection>
     </styles.BookingWrapper>
   );
