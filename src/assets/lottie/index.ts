d642a80abdd1fcfd1d8c855478a20f89f990ea82
zizonyoungjun <naya700@naver.com>
2023-08-06 18:39:51 +0900

    Design: driver icon 추가

--- src/assets/lottie/index.ts
+++ src/assets/lottie/index.ts
@@ -3,3 +3,4 @@ export { default as IcHighFive } from './IcHighFive.json';
 export { default as IcCancelled } from './IcCancelled.json';
 export { default as IcSadFace } from './IcSadFace.json';
 export { default as IcSuccess } from './IcSuccess.json';
+export { default as IcDriver } from './IcDriver.json';
