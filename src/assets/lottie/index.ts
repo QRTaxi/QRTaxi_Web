210841be57b3cf27308004035c925f3e2d168e23
zizonyoungjun <naya700@naver.com>
2023-08-06 12:42:45 +0900

    Feat: IcSuccess 아이콘 추가

--- src/assets/lottie/index.ts
+++ src/assets/lottie/index.ts
@@ -2,3 +2,4 @@ export { default as IcTaxi } from './IcTaxi.json';
 export { default as IcHighFive } from './IcHighFive.json';
 export { default as IcCancelled } from './IcCancelled.json';
 export { default as IcSadFace } from './IcSadFace.json';
+export { default as IcSuccess } from './IcSuccess.json';
