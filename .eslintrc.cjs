998962b031ea62fd16900d29c82b0ab2ad30dd5b
HiImConan <mindol0320@naver.com>
2023-08-07 13:57:20 +0900

    Docs: typescript-eslint/naming-convention 에러 해제

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -37,6 +37,13 @@ module.exports = {
     ],
     'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
     '@typescript-eslint/no-shadow': 'off', // useState의 state를 jsx 내부에서 사용할 때 shadowing으로 잘못 간주하는 버그를 off하기 위함
+    '@typescript-eslint/naming-convention': [
+      'warn',
+      {
+        selector: 'variable',
+        format: ['camelCase', 'PascalCase', 'UPPER_CASE', 'snake_case'],
+      },
+    ],
     'import/extensions': [
       'error',
       'ignorePackages',
@@ -45,7 +52,7 @@ module.exports = {
         jsx: 'never',
         ts: 'never',
         tsx: 'never',
-        "": "never"
+        '': 'never',
       },
     ],
   },
