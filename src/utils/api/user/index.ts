44be4bd010a281e0566c217942ce255ac74b8571
HiImConan <mindol0320@naver.com>
2023-08-13 16:37:17 +0900

    Refactor: Failed Response 분기

--- src/utils/api/user/index.ts
+++ src/utils/api/user/index.ts
@@ -10,74 +10,77 @@ import {
   CheckStatusPayload,
   CheckStatusResponse,
   CheckStatusFailedResponse,
+  UserInfoFailedResponse,
+  DriverInfoFailedResponse,
+  CancelBookingFailedResponse,
 } from '@/utils/types/user';
 import client from '../axios';
-import { AxiosError, AxiosResponse, isAxiosError } from 'axios';
+import { AxiosError, AxiosResponse } from 'axios';
 
 class UserApi {
   static async getUserLocation(qrID: UserQRID) {
     try {
-      const response = await client.get<UserLocationResponse>(
+      const response: AxiosResponse<UserLocationResponse> = await client.get(
         `/call/main/${qrID}`,
       );
       return response.data;
     } catch (error) {
-      if (isAxiosError<UserLocationResponse>(error)) {
-        console.log(error.message);
-        return null;
+      const axiosError = error as AxiosError<UserLocationResponse>;
+      if (axiosError.response) {
+        return axiosError.response.data;
+      } else {
+        throw new Error();
       }
-      console.log('Unexpected error', error);
-      return null;
     }
   }
 
   static async postUserInfo(payload: UserInfoPayload) {
     try {
-      const response = await client.post<UserInfoResponse>(
+      const response: AxiosResponse<UserInfoResponse> = await client.post(
         `/call/main/${payload.hashed_qr_id}`,
         payload,
       );
       return response.data;
     } catch (error) {
-      if (isAxiosError<UserInfoResponse>(error)) {
-        console.log(error.message);
-        return null;
+      const axiosError = error as AxiosError<UserInfoFailedResponse>;
+      if (axiosError.response) {
+        return axiosError.response.data;
+      } else {
+        throw new Error();
       }
-      console.log('Unexpected error', error);
-      return null;
     }
   }
 
   static async getDriverInfo(hashed_assign_id: DriverInfoPayload) {
     try {
-      const response = await client.get<DriverInfoResponse>(
+      const response: AxiosResponse<DriverInfoResponse> = await client.get(
         `/call/success/${hashed_assign_id}`,
       );
       return response.data;
     } catch (error) {
-      if (isAxiosError<DriverInfoResponse>(error)) {
-        console.log(error.message);
-        return null;
+      const axiosError = error as AxiosError<DriverInfoFailedResponse>;
+      if (axiosError.response) {
+        return axiosError.response.data;
+      } else {
+        throw new Error();
       }
-      console.log('Unexpected error', error);
-      return null;
     }
   }
 
   static async postCancelBooking(payload: CancelBookingPayload) {
     try {
-      const response = await client.post<CancelBookingResponse>(
+      const response: AxiosResponse<CancelBookingResponse> = await client.post(
         `/call/cancel/`,
         payload,
       );
       return response.data;
     } catch (error) {
-      if (isAxiosError<CancelBookingResponse>(error)) {
-        console.log(error.message);
-        return null;
+      const axiosError = error as AxiosError<CancelBookingFailedResponse>;
+      if (axiosError.response) {
+        return axiosError.response.data;
+      } else {
+        throw new Error();
       }
-      console.log('Unexpected error', error);
-      return null;
     }
   }
 
