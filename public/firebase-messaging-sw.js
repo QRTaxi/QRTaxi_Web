4e2a757b29224a09cbfe7872cb3c70dae9b710a6
HiImConan <mindol0320@naver.com>
2023-08-16 16:31:09 +0900

    Feat: 백그라운드 푸시 알림 서비스워커 추가 및 webworker lib추가

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,42 @@
+/// <reference lib="webworker" />
+
+export default null;
+
+self.addEventListener('install', event => {
+  console.log('fcm sw install..');
+  event.waitUntil(self.skipWaiting());
+  // .catch((error: Error) => console.error(error))
+});
+
+self.addEventListener('activate', () => {
+  console.log('fcm sw activate..');
+});
+
+self.addEventListener('push', event => {
+  if (!event.data) return;
+
+  const message = event.data.json();
+  const notificationOptions = {
+    body: message.notification.body,
+    icon: '/icons/QT_initial_v1.svg',
+    data: { url: `${self.location.origin}/${message.data.status}` },
+  };
+  event.waitUntil(
+    self.registration.showNotification(
+      message.notification.title,
+      notificationOptions,
+    ),
+  );
+});
+
+self.addEventListener('notificationclick', event => {
+  // Close the notification popout
+  event.notification.close();
+  // Get all the Window clients
+  event.waitUntil(
+    clients
+      .openWindow(event.notification.data.url)
+      .then(windowClient => (windowClient ? windowClient.focus() : null))
+      .catch(error => console.error(error)),
+  );
+});
