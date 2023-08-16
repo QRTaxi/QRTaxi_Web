5336b7556985801ec7d89f91fe5761cd1686b483
HiImConan <mindol0320@naver.com>
2023-08-16 16:34:11 +0900

    Feat: 브라우저 푸시 알림 허용 요청 로직 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,35 @@ import GlobalStyle from 'src/styles/globalStyles';
 import { ThemeProvider } from 'styled-components';
 import { RouterProvider } from 'react-router-dom';
 import { theme } from 'src/styles/theme';
-import { Suspense } from 'react';
-import { RecoilRoot } from 'recoil';
+import { Suspense, useEffect } from 'react';
+import { useRecoilValue } from 'recoil';
 import Router from './components/common/Router';
 import Layout from './components/common/Layout';
 
+import { requestPermission } from './FirebaseConfig';
+import { userStatus } from './utils/recoil/store';
+
 export default function App() {
+  const { id: assign_id } = useRecoilValue(userStatus);
+
+  // Need this handle FCM token generation when a user manually blocks or allows notification
+  useEffect(() => {
+    if (window.Notification?.permission === 'granted') {
+      requestPermission(assign_id).catch((error: Error) =>
+        console.error(error),
+      );
+    }
+  }, []);
+
   return (
     <>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
-        <RecoilRoot>
-          <Layout>
-            <Suspense>
-              <RouterProvider router={Router} />
-            </Suspense>
-          </Layout>
-        </RecoilRoot>
+        <Layout>
+          <Suspense>
+            <RouterProvider router={Router} />
+          </Suspense>
+        </Layout>
       </ThemeProvider>
     </>
   );
