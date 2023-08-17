822509ea3842093ff282f0f88f435981f1c8a79c
HiImConan <mindol0320@naver.com>
2023-08-17 14:17:33 +0900

    Fix: serviceWorker register시 scope 루트 위치로 재설정

--- src/FirebaseConfig.tsx
+++ src/FirebaseConfig.tsx
@@ -33,14 +33,11 @@ export const getOrRegisterServiceWorker = () => {
     typeof window.navigator.serviceWorker !== 'undefined'
   ) {
     return window.navigator.serviceWorker
-      .getRegistration('/firebase-push-notification-scope')
+      .getRegistration()
       .then(serviceWorker => {
         if (serviceWorker) return serviceWorker;
         return window.navigator.serviceWorker.register(
           '/firebase-messaging-sw.js',
-          {
-            scope: '/firebase-push-notification-scope',
-          },
         );
       });
   }
