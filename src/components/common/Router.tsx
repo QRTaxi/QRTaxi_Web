b847092ba1ce2f11db82c164109b17fdea65a311
HiImConan <mindol0320@naver.com>
2023-08-17 18:31:02 +0900

    Fix: errorElement 추가 및 success/riding 제외 에러 연결

--- src/components/common/Router.tsx
+++ src/components/common/Router.tsx
@@ -8,32 +8,39 @@ import Finish from '@/pages/finish';
 import Failed from '@/pages/failed';
 import Success from '@/pages/success';
 import Riding from '@/pages/riding';
+import Error from '@/pages/error';
 
 const Router = createBrowserRouter([
   {
     path: '/',
     element: <Landing />,
+    errorElement: <Error />,
   },
   {
     path: 'booking/:qrID',
     element: <Booking />,
     loader: bookingLoader,
+    errorElement: <Error />,
   },
   {
     path: 'waiting',
     element: <Waiting />,
+    errorElement: <Error />,
   },
   {
     path: 'cancel',
     element: <Cancel />,
+    errorElement: <Error />,
   },
   {
     path: 'failed',
     element: <Failed />,
+    errorElement: <Error />,
   },
   {
     path: 'finish',
     element: <Finish />,
+    errorElement: <Error />,
   },
   {
     path: 'success',
