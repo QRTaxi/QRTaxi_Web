fb05c59dece0a2faaf62da4de24111bc8a4d5eb6
HiImConan <mindol0320@naver.com>
2023-08-18 22:13:20 +0900

    Design: landing 페이지 디자인 추가

---
+++ src/assets/images/capture/index.ts
@@ -0,0 +1,13 @@
+export { default as CaptureBooking } from './CaptureBooking.png';
+export { default as CaptureFinish } from './CaptureFinish.png';
+export { default as CaptureRiding } from './CaptureRiding.png';
+export { default as CaptureSuccess } from './CaptureSuccess.png';
+export { default as CaptureWaiting } from './CaptureWaiting.png';
+export { default as CaptureAndroidStep1 } from './CaptureAndroidStep1.png';
+export { default as CaptureAndroidStep2 } from './CaptureAndroidStep2.png';
+export { default as CaptureAndroidStep3 } from './CaptureAndroidStep3.png';
+export { default as CaptureAndroidStep4 } from './CaptureAndroidStep4.png';
+export { default as CaptureIOSStep1 } from './CaptureIOSStep1.png';
+export { default as CaptureIOSStep2 } from './CaptureIOSStep2.png';
+export { default as CaptureIOSStep3 } from './CaptureIOSStep3.png';
+export { default as CaptureIOSStep4 } from './CaptureIOSStep4.png';
--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,25 +1,26 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import * as styles from './LandingStyle';
+
+import { useNavigate } from 'react-router-dom';
+
+import Lottie from 'lottie-react';
+import {
+  IcArrowDown,
+  IcFingerClick,
+  IcLandingTaxi1,
+  IcReadingQR,
+} from '@/assets/lottie';
 import {
   LogoSymbolBlack,
   LogoWordmarkOrange,
   LogoLife,
-  CaptureBooking,
-  CaptureFinish,
-  CaptureRiding,
-  CaptureSuccess,
-  CaptureWaiting,
-} from '@/assets/images';
-import * as styles from './LandingStyle';
-import { useNavigate } from 'react-router-dom';
-import Lottie from 'lottie-react';
+} from '@/assets/images/logo';
 import {
-  IcLandingTaxi1,
-  IcLandingTaxi2,
-  IcArrowDown,
-  IcFingerClick,
-  IcReadingQR,
-} from '@/assets/lottie';
+  CaptureBooking,
+  CaptureIOSStep2,
+  CaptureIOSStep4,
+  CaptureAndroidStep2,
+  CaptureAndroidStep4,
+} from '@/assets/images/capture';
 
 const Landing = () => {
   const navigate = useNavigate();
@@ -30,69 +31,152 @@ const Landing = () => {
 
   return (
     <styles.LandingWrapper>
-      <styles.FirstSection style={{ justifyContent: 'center' }}>
+      <styles.FirstSection>
         <LogoWordmarkOrange />
+        <styles.Subtitle>QR코드로 한 번에! 큐택</styles.Subtitle>
       </styles.FirstSection>
-      <styles.FirstSection style={{ marginTop: '0.5rem' }}>
-        <styles.CamBtnDescription
-          style={{
-            color: '#FF8A00',
-            marginBottom: '3rem',
-            marginLeft: '3.6rem',
-          }}
-        >
-          QR코드로 한 번에 ! 큐택
-        </styles.CamBtnDescription>
-      </styles.FirstSection>
+
       <styles.SecondSection>
-        <styles.CameraButton
-          onClick={handleNewAssign}
-          style={{ marginBottom: '5rem' }}
-        >
+        <styles.CameraButton onClick={handleNewAssign}>
           <LogoSymbolBlack />
         </styles.CameraButton>
-        <styles.CamBtnDescription style={{ color: '#FF8A00' }}>
+        <styles.CamBtnDescription>
           큐택 사용법을 알고싶으시다면
           <br />
-          아래로 내려보세요 !
+          아래로 내려보세요!
         </styles.CamBtnDescription>
-        <styles.LottieSection style={{ marginTop: '0.5rem' }}>
-          <Lottie animationData={IcArrowDown} loop={false} />
-        </styles.LottieSection>
+
+        <Lottie animationData={IcArrowDown} />
       </styles.SecondSection>
-      <styles.SecondSection style={{ gap: '5rem' }}>
-        <LogoLife />
-        <styles.Contents style={{ color: '#FF8A00' }}>
-          <h1>Part 1 큐택으로 택시를 호출하는 방법</h1>
-        </styles.Contents>
-      </styles.SecondSection>
-      <styles.SecondSection>
-        <styles.CamBtnDescription style={{ color: '#FF8A00' }}>
-          Step 1
-          <br />
-          카메라를 켜 QR 코드를 인식해주세요 !
-        </styles.CamBtnDescription>
-        <styles.LottieSection>
-          <Lottie animationData={IcReadingQR} />
-        </styles.LottieSection>
-        <styles.CamBtnDescription style={{ color: '#FF8A00' }}>
-          Step 2
-          <br />
-          지금 어디인지 입력할 필요 없어요 !
-          <br />
-          기사님과 연락할 전화번호만 입력하고 호출하기를 눌러주세요
-        </styles.CamBtnDescription>
-        <styles.LottieSection>
-          <Lottie animationData={IcFingerClick} />
-        </styles.LottieSection>
-        <>
-          <styles.CamBtnDescription style={{ color: '#FF8A00' }}>
-            Step 3
+
+      <styles.ThirdSection>
+        <styles.StepBlock>
+          <styles.LottieSection>
+            <LogoLife />
+          </styles.LottieSection>
+          <styles.ThirdHeader>
+            Part 1. 큐택으로
             <br />
-            그럼 편안한 여행 되세요 😉
-          </styles.CamBtnDescription>
-        </>
-      </styles.SecondSection>
+            택시를 호출하는 방법
+          </styles.ThirdHeader>
+        </styles.StepBlock>
+
+        <styles.StepBlock>
+          <styles.StepDescription>
+            <span>Step 1.</span>
+            <br />
+            <styles.bodyText>
+              카메라를 켜 QR 코드를 인식해주세요!
+            </styles.bodyText>
+          </styles.StepDescription>
+          <Lottie animationData={IcReadingQR} />
+        </styles.StepBlock>
+        <styles.StepBlock>
+          <styles.StepDescription>
+            <span>Step 2.</span>
+            <br />
+            <styles.bodyText>
+              지금 어디인지 입력할 필요 없어요!
+              <br />
+              기사님과 연락할 전화번호만 입력하고
+              <br />
+              호출하기를 눌러주세요.
+            </styles.bodyText>
+          </styles.StepDescription>
+          <styles.LottieSection>
+            <img src={CaptureBooking} alt="booking-capture" />
+            <Lottie animationData={IcFingerClick} />
+          </styles.LottieSection>
+        </styles.StepBlock>
+        <styles.StepBlock>
+          <styles.StepDescription>
+            <span>Step 3.</span>
+            <br />
+            <styles.bodyText>
+              조금만 기다리시면 기사님이 도착해요.
+              <br />
+              기사님께 직접 목적지를 말씀드리고,
+              <br />
+              하차 시에도 요금을 직접 지불하시면 됩니다. 😉
+            </styles.bodyText>
+          </styles.StepDescription>
+
+          <styles.LottieTaxiSection>
+            <Lottie animationData={IcLandingTaxi1} />
+          </styles.LottieTaxiSection>
+
+          <styles.StepDescription>
+            <styles.bodyText>그럼 안전한 여행되세요!</styles.bodyText>
+          </styles.StepDescription>
+        </styles.StepBlock>
+      </styles.ThirdSection>
+      <styles.FourthSection>
+        <styles.StepBlock>
+          <styles.ThirdHeader>
+            Part 2. 모바일 환경에서
+            <br />
+            큐택을 원활히 즐기는 방법
+          </styles.ThirdHeader>
+        </styles.StepBlock>
+        <styles.StepBlock>
+          <styles.StepDescription>
+            <span>iOS(아이폰)</span>
+          </styles.StepDescription>
+          <styles.HelpImgSection>
+            <styles.StepBlock>
+              <styles.HelpDescription>
+                1. 사파리의 &apos;공유하기&apos; 클릭 후,
+                <br />
+                &apos;홈 화면에 추가&apos;를 클릭해 큐택을 홈 화면에 추가하세요.
+              </styles.HelpDescription>
+              <styles.IOS1LottieSection>
+                <img src={CaptureIOSStep2} alt="booking-capture" />
+                <Lottie animationData={IcFingerClick} />
+              </styles.IOS1LottieSection>
+            </styles.StepBlock>
+            <styles.StepBlock>
+              <styles.HelpDescription>
+                2. 홈 화면의 큐택 앱을 실행하고,
+                <br />
+                전화번호를 입력한 뒤 알림 허용 팝업에 허용을 눌러주세요!
+              </styles.HelpDescription>
+              <styles.IOS2LottieSection>
+                <img src={CaptureIOSStep4} alt="booking-capture" />
+                <Lottie animationData={IcFingerClick} />
+              </styles.IOS2LottieSection>
+            </styles.StepBlock>
+          </styles.HelpImgSection>
+        </styles.StepBlock>
+        <styles.StepBlock>
+          <styles.StepDescription>
+            <span>안드로이드</span>
+          </styles.StepDescription>
+          <styles.HelpImgSection>
+            <styles.StepBlock>
+              <styles.HelpDescription>
+                1. 브라우저의 옵션 버튼에서
+                <br />
+                &apos;앱 설치&apos;를 클릭해 큐택을 홈 화면에 추가하세요.
+              </styles.HelpDescription>
+              <styles.Android1LottieSection>
+                <img src={CaptureAndroidStep2} alt="AndroidStep2-capture" />
+                <Lottie animationData={IcFingerClick} />
+              </styles.Android1LottieSection>
+            </styles.StepBlock>
+            <styles.StepBlock>
+              <styles.HelpDescription>
+                2. 홈 화면의 큐택 앱을 실행하고,
+                <br />
+                전화번호를 입력한 뒤 알림 허용 팝업에 허용을 눌러주세요!
+              </styles.HelpDescription>
+              <styles.Android2LottieSection>
+                <img src={CaptureAndroidStep4} alt="AndroidStep4-capture" />
+                <Lottie animationData={IcFingerClick} />
+              </styles.Android2LottieSection>
+            </styles.StepBlock>
+          </styles.HelpImgSection>
+        </styles.StepBlock>
+      </styles.FourthSection>
     </styles.LandingWrapper>
   );
 };
